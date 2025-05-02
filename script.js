8b7451317caff3a747d82934bce2fa6bd2f3f834
Tristan Adrian <tristanmendezmayorga52@gmail.com>
2025-05-02 00:27:05 -0600

    Nombre, Historia del metodo newton-raphson y manual de usuario agregados, ademas de algunas modificaciones de estilo en la pagina.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+// Funcion para hacer los cambios de seccion en la pagina al hacerles "click"
 function showSection(id) {
     document.querySelectorAll('section').forEach(sec => {
       sec.classList.remove('active');
     });
     document.getElementById(id).classList.add('active');
   }
-  
